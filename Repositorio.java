73b2e8c7b05e5621b0cb5ee52ce2caa20b8f3028
Alonso210320002 <sorianoalonso83@gmail.com>
2024-04-21 21:54:11 -0500

    Create Repositorio.java

---
+++ Repositorio.java
@@ -0,0 +1,17 @@
+public class Repositorio {
+    public static void main(String[] args) {
+        /*int[] numero = new int[4];
+         numero[0]=10;
+         numero[1]=8;
+         numero[2]=6;
+         numero[3]=11;
+         
+         for(int i=0;i<numero.length;i++){
+           System.out.println(numero[i]);
+         }*/
+         String[] numero = {"10","8","6","11" };
+           for(int i=0;i<numero.length;i++){
+           System.out.println(numero[i]);
+         }
+}
+}
